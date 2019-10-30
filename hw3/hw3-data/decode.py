7aa707c2eded21deb9d2b46ccb1cf2194a682a29
liuyuhs <56706751+liuyuhs@users.noreply.github.com>
2019-10-29 23:30:27 -0700

    Add files via upload

--- hw3/hw3-data/decode.py
+++ hw3/hw3-data/decode.py
@@ -4,7 +4,7 @@ import sys
 
 
 def viterbi(observe, states, start_probs, tran_probs, emit_probs):
-    v = [{}]
+    v = [{}, {}]
     #print(start_probs)
     #print(states)
     for st1 in states:
@@ -14,44 +14,45 @@ def viterbi(observe, states, start_probs, tran_probs, emit_probs):
                     state = (st1, st2)
                     v[0][state] = {"prob": tran_probs[('<s>', '<s>')][state] * emit_probs[st2][observe[0]], "prev": ('<s>','<s>')}
     #print(v[0])
-
-    for i in range(1, 5):
+    for st in states:
+        if (st != '<s>' and observe[1] in emit_probs[st]):
+            max_probs = -1
+            prev = 'None'
+            for prev_st in v[0]:
+                state = (prev_st[1], st)
+                prev = prev_st
+                probs =  v[0][prev_st]['prob'] * tran_probs[prev_st][state]
+                v[1][state] = {"prob": probs * emit_probs[st][observe[1]], "prev": prev}
+    #for key in v[1]:
+        #print(key,v[1][key])
+    for i in range(2, 5):
         v.append({})
         for st in states:
             if (st != '<s>' and observe[i] in emit_probs[st]):
-                max_probs = -1
-                prev = 'None'
-                next = ()
                 for prev_st in v[i-1]:
-                    state = (prev_st[1], st)
-                    tmp_probs =  v[i-1][prev_st]['prob'] * tran_probs[prev_st][state]
-                    if tmp_probs >= max_probs:
-                        max_probs = tmp_probs
-                        prev = prev_st
-                        next = state
-                #prev_state =
-                if(max_probs != -1):
-                    v[i][next] = {"prob": max_probs * emit_probs[st][observe[i]], "prev": prev}
-'''
-    for st in states:
-        if(st == '<s>' and ob)
-            max_probs = -1
-            prev = 'None'
-            next = ()
-            for prev_st in v[i-1]:
-                state = (prev_st[1], st)
-                tmp_probs =  v[i-1][prev_st]['prob'] * tran_probs[prev_st][state]
-                if tmp_probs >= max_probs:
-                    max_probs = tmp_probs
-                    prev = prev_st
-                    next = state
-            #prev_state =
-            if(max_probs != -1):
-                v[i][next] = {"prob": max_probs * emit_probs[st][observe[i]], "prev": prev}
-'''
-    for i in range(len(v)):
-        print(v[i])
-        print("-----------")
+                    state = (prev_st[1] ,st)
+                    max_probs = -1
+                    prev = 'None'
+                    for prev2_st in v[i-2]:
+                        prev_state = (prev2_st[1], prev_st[1])
+                        #prev_state = (prev2_st[1], prev_st[0])
+                        #if(state == ('AA', 'N')):
+                            #print(prev_st, v[i-1][prev_st]['prob'])
+                        #print(state)
+                        tmp_probs = v[i-1][prev_state]['prob'] * tran_probs[prev_state][state]
+                        if tmp_probs >= max_probs:
+                            max_probs = tmp_probs
+                            prev = prev_state
+                    if(max_probs != -1):
+                        v[i][state] = {"prob": max_probs * emit_probs[st][observe[i]], "prev": prev}
+    #print(v[3][('AH', 'N')])
+    #print(v[3][('AH', 'N')])
+    #for key in v[4]:
+        #print(key,v[4][key], len(v[4]))
+
+    for prev_st in v[-1]:
+        #print(prev_st)
+        v[-1][prev_st]['prob'] *= tran_probs[prev_st][(prev_st[1], '</s>')]
 
     opt = []
     # The highest probability
@@ -76,7 +77,8 @@ emit_probs = {}
 tran_probs = {}
 start_probs = {('<s>', '<s>') : 1.0}
 states = []
-observe = ('P', 'I', 'A', 'N', 'O', '</s>')
+#observe = ('P', 'I', 'A', 'N', 'O', '</s>')
+observe = ('N', 'A', 'I', 'T', 'O')
 #tran_probs
 for line in open(sys.argv[1]):
     l = line.split()
