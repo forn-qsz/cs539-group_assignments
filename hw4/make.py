2b9241d82b70af5dffeb6fe1f598be41118e0740
Jing <jshota@hotmail.com>
2019-11-10 17:52:19 -0800

    Fix: list to dict

--- hw4/make.py
+++ hw4/make.py
@@ -1,7 +1,8 @@
 from sys import stdin
+from collections import defaultdict
 
-#epron = ['W', 'AY', 'N']
-#jpron = ['W', 'A', 'I', 'N']
+epron = ['W', 'AY', 'N']
+jpron = ['W', 'A', 'I', 'N']
 
 #epron = ['AY', 'N']
 #jpron = ['I', 'N']
@@ -12,29 +13,27 @@ from sys import stdin
 #epron = ['B', 'OW', 'T']
 #jpron = ['B', 'O', 'O', 'T', 'O']
 
-epron = ['T', 'EH', 'S', 'T']
-jpron = ['T', 'E', 'S','U', 'T', 'O']
+#epron = ['T', 'EH', 'S', 'T']
+#jpron = ['T', 'E', 'S', 'U', 'T', 'O']
 
 start = [0.333, 0.333, 0.333]
-def enum(epron, jpron, path):
+
+def enum(epron, jpron, path, count=0):
     if(len(epron) == 1):
         k = []
         for i in range(len(jpron)):
             k.append(jpron[i])
-        path.append([epron, k])
-        #print(prev, 'haha')
+        path[(epron[0], count)] = k
         print (path)
+        count = 0
     else:
-        #print()
         for i in range(len(jpron) - len(epron) + 1):
             k = []
-            test = []
             for j in range(i + 1):
                 k.append(jpron[j])
-            #print(epron[0], k)
-            test.append([epron[0], k])
-            enum(epron[1:], jpron[i+1:], test)
-path = []
+            path[(epron[0], count)] = k
+            enum(epron[1:], jpron[i+1:], path, count + 1)
+path = defaultdict(dict)
 enum(epron, jpron, path)
 
 #dic = {'W': {'W' : 0.0, 'WA' : 0.0}, 'AY': {'A' : 0.0, 'AI', 'I':0.0}, 'N':
