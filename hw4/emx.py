3e80c878af5a1e034a0180b6a7ff9a3fc6110349
liuyuhs <56706751+liuyuhs@users.noreply.github.com>
2019-11-11 18:28:31 -0800

    Add files via upload

--- hw4/emx.py
+++ hw4/emx.py
@@ -6,26 +6,21 @@ from collections import Counter
 epron = []
 jpron = []
 
-#epron = ['B', 'OW', 'T']
-#jpron = ['B', 'O', 'O', 'T', 'O']
-
-#epron = ['T', 'EH', 'S', 'T']
-#jpron = ['T', 'E', 'S','U', 'T', 'O']
 all_path = []
+fractional_counts = []
 
 def normalize(probs):
     prob_factor = 1 / sum(probs)
     return [prob_factor * p for p in probs]
 
-def enum(epron, jpron, path, ek_pair, count=0):
+def enum(epron, jpron, path, ek_pair, pair, count=0):
     if(len(epron) == 1):
         k = []
         for i in range(len(jpron)):
             k.append(jpron[i])
         path[(epron[0], count)] = ''.join(k)
         ek_pair[epron[0]][''.join(k)] = 0
-        all_path.append(path.copy())
-        #print (path)
+        all_path[pair].append(path.copy())
         count = 0
     else:
         for i in range(len(jpron) - len(epron) + 1):
@@ -34,7 +29,7 @@ def enum(epron, jpron, path, ek_pair, count=0):
                 k.append(jpron[j])
             path[(epron[0], count)] = ''.join(k)
             ek_pair[epron[0]][''.join(k)] = 0
-            enum(epron[1:], jpron[i+1:], path, ek_pair, count + 1)
+            enum(epron[1:], jpron[i+1:], path, ek_pair, pair, count + 1)
 def ini_ek(ek_pair):
     for e in ek_pair:
         for j in ek_pair[e]:
@@ -47,9 +42,11 @@ def em(fractional_counts, ek_pair):
     while(1 - corpus_prob[iterative] > 0.01 and iterative < 20):
         ini_ek(ek_pair)
         for i in range(len(all_path)):
-            for j in all_path[i]:
-                ek_pair[j[0]][all_path[i][j]] += fractional_counts[i]['prob']
-        #print(ek_pair)
+            for j in range(len(all_path[i])):
+                for k in all_path[i][j]:
+                    #print(k[0], all_path[i][j][k])
+                    ek_pair[k[0]][all_path[i][j][k]] += fractional_counts[i][j]['prob']
+
         for e in ek_pair:
             p_ej = []
             for j in ek_pair[e]:
@@ -60,22 +57,31 @@ def em(fractional_counts, ek_pair):
             for j in ek_pair[e]:
                 ek_pair[e][j] = p_ej[ind]
                 ind += 1
-        p_xz = []
+        #print(ek_pair)
+
+        c_prob = []
         for i in range(len(fractional_counts)):
-            prob = float(1)
-            for j in range(len(epron)):
-                jp = fractional_counts[i]['path'][j]
-                #print(ek_pair[epron[j]][jp])
-                prob *= ek_pair[epron[j]][jp]
-            fractional_counts[i]['prob'] = prob  #regenerate
-            p_xz.append(prob)
+            p_xz = []
+            for j in range(len(fractional_counts[i])):
+                prob = float(1)
+                for k in range(len(epron[i])):
+                    jp = fractional_counts[i][j]['path'][k]
+                    #print(epron[i][k] ,jp)
+                    #print(ek_pair[epron[i][k]][jp])
+                    prob *= ek_pair[epron[i][k]][jp]
+                fractional_counts[i][j]['prob'] = prob  #regenerate
+                #print(fractional_counts[i][j]['prob'])
+                p_xz.append(prob)
+            c_prob.append(sum(p_xz))
+            p_xz = normalize(p_xz)
+            #print(p_xz)
+            for l in range(len(fractional_counts[i])):
+                fractional_counts[i][l]['prob'] = p_xz[l]
         #print(fractional_counts)
         #m_step
-        corpus_prob.append(sum(p_xz))
-        p_xz = normalize(p_xz)
+        corpus_prob.append(sum(c_prob))
+        #print(corpus_prob)
 
-        for i in range(len(fractional_counts)):
-            fractional_counts[i]['prob'] = p_xz[i]
         #print table
         non_zeros = 0
         print("iteration " + str(iterative) + '    ----- corpus prob = ' + str(corpus_prob[iterative]))
@@ -88,6 +94,7 @@ def em(fractional_counts, ek_pair):
                     non_zeros += 1
             print(''.join(l))
         print('nonzeros = ' + str(non_zeros))
+
         iterative += 1
 
 
@@ -96,34 +103,35 @@ index = 0
 for line in stdin:
     #print(line)
     if(index % 2 == 0):
-        #epron.append(line.split())
-        epron = line.split()
+        epron.append(line.split())
     elif(index % 2 == 1):
-        #jpron.append(line.split())
-        jpron = line.split()
+        jpron.append(line.split())
     index += 1
 
-path = defaultdict(dict)
 ek_pair = {}
 for i in epron:
-    #for j in i:
-    ek_pair[i] = {}
-#for i in range(len(epron)):
-    #enum(epron[i], jpron[i], path, ek_pair)
-enum(epron, jpron, path, ek_pair)
+    for j in i:
+        ek_pair[j] = {}
+for i in range(len(epron)):
+    path = defaultdict(dict)
+    all_path.append([])
+    enum(epron[i], jpron[i], path, ek_pair, i)
+#enum(epron, jpron, path, ek_pair)
 #print(ek_pair)
-
+#print(all_path[1])
 #initialize fractional_counts
-fractional_counts = []
+
 for i in range(len(all_path)):
-    tmp = []
-    for j in all_path[i]:
-        tmp.append(all_path[i][j])
-    fractional_counts.append({})
-    fractional_counts[i] = {'path' : tuple(tmp), 'prob' : float(0)}
+    fractional_counts.append([])
+    for j in range(len(all_path[i])):
+        tmp = []
+        for k in all_path[i][j]:
+            tmp.append(all_path[i][j][k])
+        fractional_counts[i].append({})
+        fractional_counts[i][j] = {'path' : tuple(tmp), 'prob' : float(0)}
 for i in range(len(fractional_counts)):
-    fractional_counts[i]['prob'] = 1/len(fractional_counts)
-#print(fractional_counts)
+    for j in range(len(fractional_counts[i])):
+        fractional_counts[i][j]['prob'] = 1/len(fractional_counts[i])
 
 em(fractional_counts, ek_pair)
 #print(fractional_counts)
