5af5aa251fcde16448bdc9bf3d186742baa210ab
Jing <jshota@hotmail.com>
2019-11-23 20:08:19 -0800

    Add: hw5 files and q1.a solution

---
+++ hw5/hw5-data/replace_onecounts.py
@@ -0,0 +1,107 @@
+import sys
+from collections import defaultdict
+
+logs = sys.stderr
+
+class helper:
+    open_brackets = '('
+    close_brackets = ')'
+    brackets_map = {')':'('}
+    result_dic = None
+    result_dic_list = []
+    word_count_dic = defaultdict(int)
+
+    def __init__(self):
+        ## Read while initializing
+        self.read()
+
+    def assign_result_dic(self, stack, s=None):
+        tmp_stack = stack.copy()
+        tmp_dic = self.result_dic
+        ## Assign new/old word
+        if s:
+            ## Count word appear times
+            self.word_count_dic[s] += 1
+            while(tmp_stack):
+                tmp_dic = tmp_dic[tmp_stack.pop(0)]
+            if tmp_dic[s]:
+                tmp_dic[s] += 1
+            else:
+                tmp_dic[s] = 1
+        ## Assign new tag
+        else:
+            while(len(tmp_stack)>1):
+                tmp_dic = tmp_dic[tmp_stack.pop(0)]
+            tmp_dic[tmp_stack.pop(0)] = defaultdict(dict)
+
+    def list_to_dic(self, list, stack=[], start=True):
+        while stack and list or start:
+            start = False
+            s = list.pop(0)
+            ## (TOP
+            if s[0] in self.open_brackets:
+                stack.append(s[1:])
+                ## Assign tags in stack into result_dic
+                self.assign_result_dic(stack)
+                ## Iterate until found a word
+                self.list_to_dic(list, stack, start)
+            # Does)
+            else:
+                if len(stack) < 1:
+                    sys.exit("Parentheses not balanced!")
+                else:
+                    ## Assign word to the current tag
+                    self.assign_result_dic(stack, s.replace(')', '').replace('\n', ''))
+                    ## Pop used tags
+                    while s[-1] == ')':
+                        s = s.replace(')', '', 1)
+                        stack.pop()
+                    break
+
+    def read(self):
+        for line in sys.stdin:
+            self.result_dic = defaultdict(dict)
+            self.list_to_dic(line.split(' '), [], True)
+            self.result_dic_list.append(self.result_dic.copy())
+
+    def enum_result_dic(self, result_dic, begin=True):
+        for k, v in result_dic.items():
+            if isinstance(v, dict):
+                if begin:
+                    print("(" + k, end='')
+                else:
+                    print(" (" + k, end='')
+                self.enum_result_dic(v, False)
+                print(')', end='')
+            else:
+                if self.word_count_dic[k] == 1:
+                    print(" <unk>", end='')
+                else:
+                    print(' ' + k, end='')
+
+    def output(self):
+        for result_dic in self.result_dic_list:
+            self.enum_result_dic(result_dic)
+            print()
+        for k, v in self.word_count_dic.items():
+            if v > 1:
+                logs.write(k + '\n')
+
+'''
+class Tree(object):
+
+    def __init__(self, lines):
+
+        self.lines = lines
+
+    def word_counts(self):
+        pass
+'''
+        
+
+def main():
+    h = helper()
+    h.output()
+
+if __name__ == "__main__":
+    main()
