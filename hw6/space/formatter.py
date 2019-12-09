e4c3877230b3fcbb8378dcbcee97f76c04cf2165
Jing <jshota@hotmail.com>
2019-12-08 20:54:10 -0800

    Chord

--- hw6/space/formatter.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-filename = 'test.txt.space_restored.nlm.base'
-
-with open(filename, 'r') as fin:
-    with open(filename + '.new', "w") as fout:
-        for line in fin:
-            fout.write(line.replace('<s>', '').replace('</s>', '').replace(' ', '').replace('_', ' '))
